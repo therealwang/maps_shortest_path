faa4d63f2e866f384da3a18383f10dbe239e2227
therealwang <yuwang1@wharton.upenn.edu>
2018-07-28 10:13:42 -0400

    parse json into np.array

--- shortest_path.py
+++ shortest_path.py
@@ -6,9 +6,40 @@ Created on Sun Feb 18 16:12:14 2018
 """
 
 import pandas as pd
+import numpy as np
 import googlemaps
+from googlemaps import distance_matrix as dm
 
 
-gmaps = googlemaps.Client(key='YOUR_KEY_HERE')
+'''
+TODO: Travelling salesman approximations
+TODO: Generate test cases
+TODO: Interpretation of generic names?
+'''
 
-print gmaps.geocode('1406 Gerard St, Rockville, MD')
+API_KEY = 'YOUR_KEY_HERE'
+gmaps = googlemaps.Client(key=API_KEY)
+
+def cleanMatrix(d, l):
+    dist = np.zeros([l,l])
+    dur = np.zeros([l,l])
+    for r, i in zip(d['rows'],range(l)):
+        for c, j in zip(r['elements'], range(l)):
+            dist[i][j] = c['distance']['value']
+            dur[i][j] = c['duration']['value']
+    return dist, dur
+        
+    
+def distanceMatrix(addresses):
+    raw_matrix = dm.distance_matrix(gmaps, addresses, addresses)
+    return cleanMatrix(raw_matrix, len(addresses))
+
+
+def main():
+    l = ['3737 Chestnut St, Philadelphia, PA 19104',
+         '401 E City Ave, Bala Cynwyd, PA',
+         '1900 Arch St, Philadelphia, PA']
+    dist, dur = distanceMatrix(l)
+    return dist, dur
+    
+print main()
