a54f460c871510d5723e05347c6ddf0526ba3c7d
Yu Wang <yuwang1@wharton.upenn.edu>
2018-08-14 18:08:37 -0400

    test cases, closing hours

--- test.py
+++ test.py
@@ -5,11 +5,15 @@ Created on Sun Jul 29 19:34:23 2018
 @author: yuwan
 """
 
+'''
+TODO: make test cases more readable
+'''
 
 import numpy as np
 import timeit
 from test_cases import *
 from shortest_path import *
+import TSP_closed
 
 print travelingSalesman_naive(test_array, addresses) 
 #2236
@@ -45,3 +49,35 @@ print travelingSalesman_nearest(test_array_larger, addresses_larger)
 print timeit.timeit('lambda x: travelingSalesman_nearest(test_array_larger, addresses_larger)', 
               number = 1000)
 #doesnt get the right answer, but even for n = 7 is faster
+
+
+
+test_array = np.array([[   0.,  910.,  648.],
+       [ 954.,    0.,  797.],
+       [ 630.,  634.,    0.]])
+
+duration = [0,0]
+closing_1 = [1e5, 1e5]
+closing_2 = [910, 1e5]
+
+#in this case, we test when there is no closing time, and when location 1 closes
+#much earlier. we expect to have to take the longer path (to make sure we get
+#to location 1 before it closes)
+
+print TSP_closed.travelingSalesman_closing(test_array, ['Home','Loc1','Loc2'], 
+                                     duration, closing_1) 
+#(2236.0, ['Home', 'Loc2', 'Loc1'])
+
+
+print TSP_closed.travelingSalesman_closing(test_array, ['Home','Loc1','Loc2'], 
+                                     duration, closing_2) 
+
+#(2337.0, ['Home', 'Loc1', 'Loc2'])
+
+#What if there is no valid path?
+
+closing_3 = [1, 1e5]
+
+print TSP_closed.travelingSalesman_closing(test_array, ['Home','Loc1','Loc2'], 
+                                     duration, closing_3) 
+#(-1, [])
